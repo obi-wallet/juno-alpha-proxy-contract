7d882747d7cd934dfe1a8eafdf2c9e33893cedfd
ntropian <pete@deviantsfactions.com>
2022-08-21 17:47:16 +0000

    typo

--- scripts/test_contract.sh
+++ scripts/test_contract.sh
@@ -10,6 +10,7 @@ GAS3=--gas-adjustment=1.3
 CONTRACT_ADDRESS=juno1gsavkpywfr842hf2hcu3vs95zncu5g0ckw84twplnqupmtwx9hqqduu3zk
 CONTRACT_ADMIN_WALLET=testnet-valley
 BAD_WALLET=public
+BAD_WALLET_ADDRESS=$($BINARY keys show $BAD_WALLET --address)
 
 # fund the msig so it can send
 echo "Funding the multisig..."
@@ -30,14 +31,14 @@ $BINARY tx wasm execute $CONTRACT_ADDRESS "$EXECUTE_ARGS" --from=$BAD_WALLET --n
 # RM the new hot wallet (we'll add it back with higher spend limit)
 echo "TX 2a... try to remove hot wallet in case a previous run terminated early."
 echo "Should fail in other cases."
-RM_HOT_WALLET_ARGS=$(jq -n --arg doomed $BAD_WALLET '{"rm_hot_wallet": {"doomed_hot_wallet": "$doomed"}}')
+RM_HOT_WALLET_ARGS=$(jq -n --arg doomed $BAD_WALLET_ADDRESS '{"rm_hot_wallet": {"doomed_hot_wallet": "$doomed"}}')
 $BINARY tx wasm execute $CONTRACT_ADDRESS "$RM_HOT_WALLET_ARGS" --from=$CONTRACT_ADMIN_WALLET --node=$RPC --chain-id=$CHAIN_ID $GAS1 $GAS2 $GAS3
 
 # Add that other wallet as hot wallet for an hour
 SECS_SINCE_EPOCH=$(date +%s)
 let RESET_TIME=$SECS_SINCE_EPOCH+3600
 echo "TX 3) Admin adds a new hot wallet. Should succeed."
-ADD_HOT_WALLET_ARGS=$(jq -n --arg newaddy $BAD_WALLET --arg reset $RESET_TIME '{"add_hot_wallet": {"new_hot_wallet": {"address":"$newaddy", "current_period_reset":$reset, "period_type":"DAILY", "period_multiple":1, "spend_limits":{"coin_limit":{"denom":"ujunox","amount":"10000"},"limit_remaining":"10000"}}}}')
+ADD_HOT_WALLET_ARGS=$(jq -n --arg newaddy $BAD_WALLET_ADDRESS --arg reset $RESET_TIME '{"add_hot_wallet": {"new_hot_wallet": {"address":$newaddy, "current_period_reset":$reset, "period_type":"DAILY", "period_multiple":1, "spend_limits":{"coin_limit":{"denom":"ujunox","amount":"10000"},"limit_remaining":"10000"}}}}')
 echo "Arguments: $ADD_HOT_WALLET_ARGS"
 $BINARY tx wasm execute $CONTRACT_ADDRESS "$ADD_HOT_WALLET_ARGS" --from=$CONTRACT_ADMIN_WALLET --node=$RPC --chain-id=$CHAIN_ID $GAS1 $GAS2 $GAS3
 
@@ -48,7 +49,7 @@ $BINARY tx wasm execute $CONTRACT_ADDRESS "$EXECUTE_ARGS" --from=$BAD_WALLET --n
 
 # RM the new hot wallet (we'll add it back with higher spend limit)
 echo "TX 5) Admin removes the hot wallet. Should succeed."
-RM_HOT_WALLET_ARGS=$(jq -n --arg doomed $BAD_WALLET '{"rm_hot_wallet": {"doomed_hot_wallet": "$doomed"}}')
+RM_HOT_WALLET_ARGS=$(jq -n --arg doomed $BAD_WALLET_ADDRESS '{"rm_hot_wallet": {"doomed_hot_wallet": "$doomed"}}')
 $BINARY tx wasm execute $CONTRACT_ADDRESS "$RM_HOT_WALLET_ARGS" --from=$CONTRACT_ADMIN_WALLET --node=$RPC --chain-id=$CHAIN_ID $GAS1 $GAS2 $GAS3
 
 # error should once again read that this wallet doesn't have any hot wallet privs
@@ -62,7 +63,7 @@ sleep 10s
 echo "TX 7) Admin adds the hot wallet back, with a higher limit. Should succeed."
 SECS_SINCE_EPOCH=$(date +%s)
 let RESET_TIME=$SECS_SINCE_EPOCH+36
-ADD2_HOT_WALLET_ARGS=$(jq -n --arg newaddy $BAD_WALLET --arg reset $RESET_TIME '{"add_hot_wallet": {"new_hot_wallet": {"address":"$newaddy", "current_period_reset":$reset, "period_type":"DAILY", "period_multiple":1, "spend_limits":{"coin_limit":{"denom":"ujunox","amount":"45000"},"limit_remaining":"45000"}}}}')
+ADD2_HOT_WALLET_ARGS=$(jq -n --arg newaddy $BAD_WALLET_ADDRESS --arg reset $RESET_TIME '{"add_hot_wallet": {"new_hot_wallet": {"address":"$newaddy", "current_period_reset":$reset, "period_type":"DAILY", "period_multiple":1, "spend_limits":{"coin_limit":{"denom":"ujunox","amount":"45000"},"limit_remaining":"45000"}}}}')
 $BINARY tx wasm execute $CONTRACT_ADDRESS "$ADD2_HOT_WALLET_ARGS" --from=$CONTRACT_ADMIN_WALLET --node=$RPC --chain-id=$CHAIN_ID $GAS1 $GAS2 $GAS3
 
 echo "Please complete the follow two transactions within 60 seconds so we can test reset."
