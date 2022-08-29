2f52c309d0ba14f5bd87cae327c0338aa818341f
ntropian <pete@deviantsfactions.com>
2022-08-29 15:29:26 +0000

    key import scripts and first try at action

---
+++ scripts/import_key.sh
@@ -0,0 +1,2 @@
+junod keys add $1 --recover --keyring-backend=test
+junod keys add $2 --recover --keyring-backend=test
---
+++ scripts/import_keys.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/expect -f
+
+set timeout -1
+spawn ./scripts/import_key.sh scripttest scripttest2
+
+expect "> Enter your bip39 mnemonic\r"
+
+send -- "$env(BASHTESTER)\r"
+
+expect "> Enter your bip39 mnemonic\r"
+
+send -- "$env(BADTESTER)\r"
+
+expect eof
