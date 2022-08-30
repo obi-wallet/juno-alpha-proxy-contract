0984e390ff4ef0ad07705b302aa4616eefa1d054
ntropian <pete@deviantsfactions.com>
2022-08-30 18:25:19 +0000

    dl and install cosmwasm go helpers

---
+++ scripts/install_cosmwasm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://www.dropbox.com/s/e17oj0hbiv3wx7w/cosmwasm.tar.gz?dl=0
+mv ./cosmwasm.tar.gz?dl=0 ./cosmwasm.tar.gz
+mkdir /home/runner/go/pkg/mod/github.com
+tar xvf ./cosmwasm.tar.gz -C /home/runner/go/pkg/mod/github.com
--- scripts/install_juno.sh
+++ scripts/install_juno.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export GOROOT=/home/runner/goinstall/go
+export GOPATH=/home/runner/go
+export GO111MODULE=on
+export PATH=$PATH:/usr/local/goinstall/go/bin:/home/runner/go/bin
+
 wget https://www.dropbox.com/s/44fig40y3825g8u/junod?dl=1
 mv ./junod?dl=1 ./junod
 chmod +x ./junod
