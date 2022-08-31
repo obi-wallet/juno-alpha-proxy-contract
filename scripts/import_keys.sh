a5509916a0c3cf98b8f09ee2b4395276d55d83f7
ntropian <pete@deviantsfactions.com>
2022-08-31 14:06:34 +0000

    rm bash command in expect spawn

--- scripts/import_keys.sh
+++ scripts/import_keys.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/expect -f
 
 set timeout -1
-spawn bash ./scripts/import_key.sh scripttest scripttest2
+spawn ./scripts/import_key.sh scripttest scripttest2
 
 expect "> Enter your bip39 mnemonic\r"
 
