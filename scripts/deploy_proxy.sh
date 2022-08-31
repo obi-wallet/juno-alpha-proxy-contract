f4f47d8600586dc3a23797d52595d798ca5b6f9a
ntropian <pete@deviantsfactions.com>
2022-08-31 16:07:01 +0000

    docker install

--- scripts/deploy_proxy.sh
+++ scripts/deploy_proxy.sh
@@ -85,7 +85,7 @@ echo ""
 echo -n "Optimizing smart contract code..."
 # compile
 RES=$(docker run --rm -v "$(pwd)":/code \
-  --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+  --mount type=volume,source="$(/usr/bin/basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   cosmwasm/rust-optimizer:0.12.5)
 echo " Done."
