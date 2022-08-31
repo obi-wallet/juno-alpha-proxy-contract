edb0e67391c206b946742c5b3e5c211159b2a8b5
ntropian <pete@deviantsfactions.com>
2022-08-31 14:24:37 +0000

    try sudo

--- scripts/install_cosmwasm.sh
+++ scripts/install_cosmwasm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget https://www.dropbox.com/s/e17oj0hbiv3wx7w/cosmwasm.tar.gz?dl=0
 mv ./cosmwasm.tar.gz?dl=0 ./cosmwasm.tar.gz
-mkdir -p $GOPATH/pkg/mod/github.com
-tar xvf ./cosmwasm.tar.gz -C $GOPATH/pkg/mod/github.com
+sudo mkdir -p $GOPATH/pkg/mod/github.com
+sudo tar xvf ./cosmwasm.tar.gz -C $GOPATH/pkg/mod/github.com
