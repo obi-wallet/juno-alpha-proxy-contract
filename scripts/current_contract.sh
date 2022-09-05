a83036bdc5844cf53a7015122e0868fe68490586
ntropian <pete@deviantsfactions.com>
2022-09-05 17:47:53 +0000

    use environment vars for code id

--- scripts/current_contract.sh
+++ scripts/current_contract.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-CONTRACT_CODE=-1
+# testnet - pulled from github environment usually
+# CONTRACT_CODE=3434
+# mainnet - pulled from github environment usually
+# CONTRACT_CODE=838
 CONTRACT_ADDRESS=juno1uf5303f78u9ssjewh4f83sf22dmqzlp9c7rgyd3wldusx429mfjs9lcftp
 CONTRACT_ADMIN_WALLET=juno1ad2ykdjntnkjcer760lefkj7dx38jlj3z858sr
