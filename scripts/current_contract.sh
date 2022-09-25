564d398a709ee0c5bbb9a7c24cc069f53fc7b736
ntropian <pete@deviantsfactions.com>
2022-09-25 22:40:00 +0000

    typo

--- scripts/current_contract.sh
+++ scripts/current_contract.sh
@@ -3,6 +3,6 @@
 # CONTRACT_CODE=4135
 # mainnet - pulled from github environment usually
 # CONTRACT_CODE=1060
-CONTRACT_CODE_2=1069 // same except version, for migration testing
+CONTRACT_CODE_2=1069 #same except version, for migration testing
 CONTRACT_ADDRESS=juno1uf5303f78u9ssjewh4f83sf22dmqzlp9c7rgyd3wldusx429mfjs9lcftp
 CONTRACT_ADMIN_WALLET=juno1ad2ykdjntnkjcer760lefkj7dx38jlj3z858sr
